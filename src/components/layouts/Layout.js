ef6b88eec092447bd9f23c862c6dc0e0b2b33319
reilee21 <reilee2104@gmail.com>
2023-10-27 23:33:18 +0700

    update

---
+++ src/components/layouts/Layout.js
@@ -0,0 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Header from "./Header"
+
+
+const DefaultLayout = () =>{
+    return(
+        <>
+        <Header/>
+       <div className="container">
+              <Outlet/>
+       </div>
+        </>
+    )
+}
+
+export default DefaultLayout
