a83dcb7fe312d804b4b4da59cfbf5b6d21878919
Hoddy0812 <Thanhpham.15112003pl@gmail.com>
2023-11-07 01:11:23 +0700

    làm được ưu đãi

--- src/page/home/ImageSlider.js
+++ src/page/home/ImageSlider.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPlane, faHotel, faSearch } from '@fortawesome/free-solid-svg-icons';
+
 import { Slide } from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
 import Image1 from '../../assets/image/dulich1.png';
 import Image2 from '../../assets/image/dulich2.png';
 import Image3 from '../../assets/image/dulich3.png';
-import OfferImage1 from '../../assets/image/dulich3.png';
-import OfferImage2 from '../../assets/image/dulich3.png';
+import OfferImage1 from '../../assets/image/uudai1.jpg';
+import OfferImage2 from '../../assets/image/uudai2.jpg';
+import OfferImage3 from '../../assets/image/uudai3.jpg';
+import OfferImage4 from '../../assets/image/uudai4.jpg';
+import OfferImage5 from '../../assets/image/uudai5.jpg';
 import './ImageSlider.css';
 
 const slideImages = [
@@ -20,15 +27,35 @@ const slideImages = [
     {
         url: Image3,
     },
+];
+
+// Ưu đãi
+const offerSliderSettings = {
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 3000,
+};
+
+const offerImages = [
     {
-        url: OfferImage1, // Đường dẫn đến hình ảnh ưu đãi
+        url: OfferImage1,
     },
     {
-        url: OfferImage2, // Đường dẫn đến hình ảnh ưu đãi
+        url: OfferImage2,
     },
     {
-        url: OfferImage2, // Đường dẫn đến hình ảnh ưu đãi
+        url: OfferImage3,
     },
+    {
+        url: OfferImage4,
+    },
+    {
+        url: OfferImage5,
+    },
+    // Thêm ảnh ưu đãi khác nếu cần
 ];
 
 const ImageSlider = () => {
@@ -55,19 +82,19 @@ const ImageSlider = () => {
                 <div className="feature-container">
                     <div className="feature">
                         <div className="feature-icon">
-                            <FontAwesomeIcon icon={faPlane} size="4x" />
+                            <FontAwesomeIcon icon={faPlane} size="3x" />
                         </div>
                         <p>Đặt Tour</p>
                     </div>
                     <div className="feature">
                         <div className="feature-icon">
-                            <FontAwesomeIcon icon={faHotel} size="4x" />
+                            <FontAwesomeIcon icon={faHotel} size="3x" />
                         </div>
                         <p>Đặt Khách Sạn</p>
                     </div>
                     <div className="feature" onClick={toggleSearch}>
                         <div className="feature-icon">
-                            <FontAwesomeIcon icon={faSearch} size="4x" />
+                            <FontAwesomeIcon icon={faSearch} size="3x" />
                         </div>
                         <p>Tra cứu booking</p>
 
@@ -79,6 +106,17 @@ const ImageSlider = () => {
                         <button>Tìm kiếm</button>
                     </div>
                 )}
+            </div>
+
+            <div className="offer-slider">
+                <h2>Ưu đãi</h2>
+                <Slide {...offerSliderSettings} arrows={false} dots={true}>
+                    {offerImages.map((offerImage, index) => (
+                        <div key={index} className="image-slide1">
+                            <img src={offerImage.url} alt={`Offer Image ${index}`} />
+                        </div>
+                    ))}
+                </Slide>
             </div>
         </div>
     );
