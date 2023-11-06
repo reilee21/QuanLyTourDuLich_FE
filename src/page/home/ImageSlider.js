6f3df445dabcfd0969ed85952faa0eeb35cd5895
Hoddy0812 <Thanhpham.15112003pl@gmail.com>
2023-11-06 23:44:51 +0700

    hmmm đang bí

--- src/page/home/ImageSlider.js
+++ src/page/home/ImageSlider.js
@@ -1,32 +1,85 @@
-import React from 'react';
-import Slider from 'react-slick';
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import React, { useState } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlane, faHotel, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import Image1 from '../../assets/image/dulich1.png';
+import Image2 from '../../assets/image/dulich2.png';
+import Image3 from '../../assets/image/dulich3.png';
+import OfferImage1 from '../../assets/image/dulich3.png';
+import OfferImage2 from '../../assets/image/dulich3.png';
 import './ImageSlider.css';
-import Image1 from '../../assets/image/faq.png';  // Import your images
+
+const slideImages = [
+    {
+        url: Image1,
+    },
+    {
+        url: Image2,
+    },
+    {
+        url: Image3,
+    },
+    {
+        url: OfferImage1, // Đường dẫn đến hình ảnh ưu đãi
+    },
+    {
+        url: OfferImage2, // Đường dẫn đến hình ảnh ưu đãi
+    },
+    {
+        url: OfferImage2, // Đường dẫn đến hình ảnh ưu đãi
+    },
+];
 
 const ImageSlider = () => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
+    const [searchVisible, setSearchVisible] = useState(false);
+
+    const toggleSearch = () => {
+        setSearchVisible(!searchVisible);
     };
 
     return (
-        <div className="image-slider">
-            <Slider {...settings}>
-                <div>
-                    <img src={Image1} alt="Image 1" />
+        <div>
+            <div className="image-slider-wrapper">
+                <div className="image-slider-container">
+                    <Slide images={slideImages} duration={3000} transitionDuration={400}>
+                        {slideImages.map((slideImage, index) => (
+                            <div key={index} className="image-slide">
+                                <img src={slideImage.url} alt={`Image ${index}`} />
+                            </div>
+                        ))}
+                    </Slide>
                 </div>
-                <div>
-                    <img src={Image1} alt="Image 2" />
+            </div>
+            <div className="home-features">
+                <div className="feature-container">
+                    <div className="feature">
+                        <div className="feature-icon">
+                            <FontAwesomeIcon icon={faPlane} size="4x" />
+                        </div>
+                        <p>Đặt Tour</p>
+                    </div>
+                    <div className="feature">
+                        <div className="feature-icon">
+                            <FontAwesomeIcon icon={faHotel} size="4x" />
+                        </div>
+                        <p>Đặt Khách Sạn</p>
+                    </div>
+                    <div className="feature" onClick={toggleSearch}>
+                        <div className="feature-icon">
+                            <FontAwesomeIcon icon={faSearch} size="4x" />
+                        </div>
+                        <p>Tra cứu booking</p>
+
+                    </div>
                 </div>
-                <div>
-                    <img src={Image1} alt="Image 3" />
-                </div>
-            </Slider>
+                {searchVisible && (
+                    <div className="search-bar">
+                        <input type="text" placeholder="Nhập số booking" />
+                        <button>Tìm kiếm</button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
