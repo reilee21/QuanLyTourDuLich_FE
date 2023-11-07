ef1cc562fa7e4ab74adfb92feeff58f847c4e87c
reilee21 <reilee2104@gmail.com>
2023-11-07 20:47:09 +0700

    update

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,18 +1,15 @@
+import HomePage from "../page/home";
+import LoginPage from "../page/login";
+import SearchPage from "../page/search";
+import RegisterPage from "../page/register";
 
-import HomePage from "../page/home"
-import LoginPage from "../page/login"
-import SearchPage from "../page/search"
-import RegisterPage from "../page/register"
+import Cus_Infro from "../page/cus_infor";
+import DoiThuong from "../page/doithuong";
+import Histour from "../page/histour";
+import Repass from "../page/repass";
+import Contact from "../page/contact";
 
-import Cus_Infro from "../page/cus_infor"
-import DoiThuong from "../page/doithuong"
-import Histour from "../page/histour"
-import Repass from "../page/repass"
-import Contact from "../page/contact"
-
-import FAQ from "../page/FAQ"
-import PhuongTien from "../page/quanly/phuongtien";
-import DiaDiem from "../page/quanly/diadiem";
+import FAQ from "../page/FAQ";
 import {
   PhuongTien,
   DiaDiem,
@@ -22,18 +19,17 @@ import {
 } from "../page/quanly/export";
 
 const defaultRoute = [
-    { path: '/', component: HomePage },
-    { path: '/login', component: LoginPage },
-    { path: '/search', component: SearchPage },
-    { path: '/register', component: RegisterPage },
-    { path: '/cus_info', component: Cus_Infro },
-    { path: '/doithuong', component: DoiThuong },
-    { path: '/histour', component: Histour },
-    { path: '/repass', component: Repass },
-    { path: '/contact', component: Contact },
-    { path: '/FAQ', component: FAQ },
-]
-
+  { path: "/", component: HomePage },
+  { path: "/login", component: LoginPage },
+  { path: "/search", component: SearchPage },
+  { path: "/register", component: RegisterPage },
+  { path: "/cus_info", component: Cus_Infro },
+  { path: "/doithuong", component: DoiThuong },
+  { path: "/histour", component: Histour },
+  { path: "/repass", component: Repass },
+  { path: "/contact", component: Contact },
+  { path: "/FAQ", component: FAQ },
+];
 
 const quanlyRoute = [
   { path: "/admin", component: HomePage },
