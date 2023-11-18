21e571cb0adcb8e6195b9c22ed8626fcc0ff0860
reilee21 <reilee2104@gmail.com>
2023-11-18 22:12:05 +0700

    1

--- src/components/layouts/Layout.js
+++ src/components/layouts/Layout.js
@@ -2,7 +2,6 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Row } from "react-bootstrap";
 import "./layout.scss";
 const DefaultLayout = () => {
   return (
